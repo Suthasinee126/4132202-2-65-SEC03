2573be2a3ff43a5b170f15f679f6e897f981e245
Suthasinee126 <640112418126@bru.ac.th>
2022-12-01 16:05:28 +0700

    LAB4

---
+++ LAB4/www/index.php
@@ -0,0 +1,33 @@
+<?php
+echo "Hello PHP !!";
+
+// phpinfo();
+
+$no = 10;
+$name = "Chompoo";
+?>
+
+<table border="1">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Dang</td>
+        </tr>
+        <?php
+        for($i = 2; $i <5; $i++) {
+        ?>
+        <tr>
+            <td><?php echo $i ?></td>
+            <td><?= $name ?></td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+</table>
---
+++ LAB4/www/process.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = $_GET['user'];
+echo "pass:" . $_GET['pass'];
+
+
+if($user == '1'){
+    echo "Hello";
+} else
+    echo $user;
+
+echo $user == 1 ? "Hello" : $user;
+
+
+
+?>
